d69078d298d75994b3a8ce10aedd85a674595c26
Guido Sejas <guidomanusejas@gmail.com>
2022-01-22 15:05:05 -0300

    Agrego otra opcion para el caso 2 // Caso 3 terminado

--- cypress/integration/casesTest.spec.js
+++ cypress/integration/casesTest.spec.js
@@ -37,11 +37,39 @@ describe("Cases to resolve", () => {
     });
   });
 
+  // it("CP002 version 2", () => {
+  //   cy.get("#layered-filter-block > .block-title > strong")
+  //     .as("filterClick")
+  //     .click();
+  //   cy.intercept("https://tienda.movistar.com.ar/?price=200000-300000").as(
+  //     "url"
+  //   );
+
+  //   cy.get('[attribute="price"] > .filter-options-content')
+  //     .contains("$200.000 y superior")
+  //     .should("contain", "$200.000 y superior")
+  //     .click();
+  //   cy.wait("@url");
+  //   cy.get("@filterClick").click();
+  //   cy.intercept(
+  //     "https://tienda.movistar.com.ar/?movistar_internalmemory=916&price=200000-300000"
+  //   ).as("url2");
+  //   cy.get('[attribute="movistar_internalmemory"]')
+  //     .contains("256GB")
+  //     .should("contain", "256GB")
+  //     .click();
+  //   cy.wait("@url2");
+  //   cy.get("#toolbar-amount").then(($span) => {
+  //     const textAmount = $span.text();
+  //     cy.log(textAmount);
+  //   });
+  // });
+
   it("CP003 - Validar cuotas en compra de equipo -Cuotas.60 -Equipo.Tercero de la lista -Banco.Credicoop -Tarjeta.Visa", () => {
     cy.get('.wrapper > .products > :nth-child(3)').click()
     cy.get('#open-installments-modal').click()
     cy.get('#selectBank').select("Credicoop").should("have.value", "Credicoop")
     cy.get('#selectCardByBank').select("Visa").should("have.value", "Visa")
-    cy.get('#bodyTable > :nth-child(1) > :nth-child()').should("have.not.text", "60")
+    cy.get('.bold-tr > td').should("not.contain", "60")
   })
 });
